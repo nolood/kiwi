7fb9524b5804f806139e2b5d5564860fcbac1fc1
nolood <a9514548799@gmail.com>
2024-07-17 17:46:41 +0300

    feat: start meilisearch app

--- cmd/meilisearch/main.go
+++ cmd/meilisearch/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"kiwi/internal/app/meilisearch"
+	"kiwi/internal/config"
+	"kiwi/internal/lib/logger"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	log := logger.New(cfg.Env)
+
+	meili := meilisearch.New(log, cfg.Meilisearch)
 
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	botapp "kiwi/internal/app/bot"
 	"kiwi/internal/app/bot/repositories"
 	"kiwi/internal/app/bot/services"
+	"kiwi/internal/app/meilisearch"
 	"kiwi/internal/config"
 	"kiwi/internal/storage/postgres"
 
@@ -11,10 +12,13 @@ import (
 )
 
 type App struct {
-	Bot *botapp.App
+	Bot     *botapp.App
+	MClient *meilisearch.App
 }
 
 func New(log *zap.Logger, cfg *config.Config) *App {
+
+	mClient := meilisearch.New(log, cfg.Meilisearch)
 
 	storage := postgres.New(cfg.Storage)
 
@@ -25,6 +29,7 @@ func New(log *zap.Logger, cfg *config.Config) *App {
 	bot := botapp.New(log, cfg.Telegram, servs)
 
 	return &App{
-		Bot: bot,
+		Bot:     bot,
+		MClient: mClient,
 	}
 }
