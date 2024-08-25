25f718180b2a5359905fd117e2e7ba6e3911963a
nolood <a9514548799@gmail.com>
2024-08-25 15:19:25 +0300

    fix: [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -13,12 +13,13 @@ RUN go build -o start-bot ./cmd/bot/main.go
 FROM ubuntu:latest
 
 COPY --from=builder /app/start-bot /usr/local/bin/start-bot
+COPY --from=builder /app/entrypoint.sh /usr/local/bin/entrypoint.sh
 COPY --from=builder /app/config /usr/local/kiwi-config
 COPY --from=builder /app/.env /usr/local/bin/.env
 COPY --from=builder /app/.env /usr/local/kiwi-config/.env
 
 RUN chmod +x /usr/local/bin/start-bot
 
-CMD ["/usr/local/bin/start-bot"]
+CMD ["/usr/local/bin/entrypoint.sh"]
 
 EXPOSE 8080
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,6 @@
 
 CONFIG_PATH="/usr/local/kiwi-config/local.yml"
 
-echo "Starting start-location..."
-/usr/local/bin/start-location -config $CONFIG_PATH &
-START_LOCATION_PID=$!
-wait $START_LOCATION_PID
+/usr/local/bin/start-bot -config=$CONFIG_PATH &
 
-echo "Starting start-meilisearch..."
-/usr/local/bin/start-meilisearch -config $CONFIG_PATH &
-START_MEILISEARCH_PID=$!
-wait $START_MEILISEARCH_PID
 
-echo "Starting start-bot..."
-/usr/local/bin/start-bot -config $CONFIG_PATH &
-START_BOT_PID=$!
-wait $START_BOT_PID
-
-echo "All applications have finished."
